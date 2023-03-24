da075ee18d59971cab918fe2e9ca0730f4cf489b
Sena <zhrsnkunt@gmail.com>
2023-03-24 18:16:37 +0300

    ZIGZAG
    When a button is pressed, numbers from 1 to 200 will be written on the screen. However, if the number is a multiple of 3, 'zig' will be written instead of the number, and if the number is a multiple of 5, 'zag' will be written instead of the number. A zigzag will be written if the number is a multiple of both 3 and 5.

---
+++ ZİGZAG/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace ZİGZAG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(26, 31);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(139, 472);
+            this.listBox1.TabIndex = 1;
+            // 
+            // guna2Button2
+            // 
+            this.guna2Button2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Button2.BorderRadius = 15;
+            this.guna2Button2.CheckedState.Parent = this.guna2Button2;
+            this.guna2Button2.CustomImages.Parent = this.guna2Button2;
+            this.guna2Button2.FillColor = System.Drawing.Color.MidnightBlue;
+            this.guna2Button2.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.guna2Button2.ForeColor = System.Drawing.Color.White;
+            this.guna2Button2.HoverState.Parent = this.guna2Button2;
+            this.guna2Button2.Location = new System.Drawing.Point(216, 197);
+            this.guna2Button2.Name = "guna2Button2";
+            this.guna2Button2.ShadowDecoration.Parent = this.guna2Button2;
+            this.guna2Button2.Size = new System.Drawing.Size(116, 45);
+            this.guna2Button2.TabIndex = 8;
+            this.guna2Button2.Text = "BAŞLAT";
+            this.guna2Button2.Click += new System.EventHandler(this.guna2Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCoral;
+            this.ClientSize = new System.Drawing.Size(405, 579);
+            this.Controls.Add(this.guna2Button2);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button2;
+    }
+}
+
---
+++ ZİGZAG/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZİGZAG
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+            int sayac = 0;
+            for (int i = 1; i <= 200; i++)
+            {
+
+
+                if (i % 3 == 0)
+                {
+                    listBox1.Items.Add(i + "ZİG");
+                }
+                if (i % 5 == 0)
+                {
+
+                    listBox1.Items.Add(i + "ZAG");
+                }
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+
+                    listBox1.Items.Add(i + "ZİGZAG");
+                }
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+
+                    listBox1.Items.Add(i + "ZİGZAG");
+                }
+                sayac++;
+            }
+        }
+    }
+}
